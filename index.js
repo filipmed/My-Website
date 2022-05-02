0b7a34c71c2c82787c931a68d79d02a3884bda3d
filipmed <74942293+filipmed@users.noreply.github.com>
2022-05-01 17:58:10 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ let menav = document.querySelector("#me");
 let projNav = document.querySelector("#proj");
 let skillNav = document.querySelector("#skills");
 
-menav.addEventListener('mouseover',()=>menav.style.color='white');
-projNav.addEventListener('mouseover',()=>projNav.style.color='white');
-skillNav.addEventListener('mouseover',()=>skillNav.style.color='white');
+menav.addEventListener('mouseover',()=>menav.style.color='#FF2E63');
+projNav.addEventListener('mouseover',()=>projNav.style.color='#FF2E63');
+skillNav.addEventListener('mouseover',()=>skillNav.style.color='#FF2E63');
 
-menav.addEventListener('mouseout',()=>menav.style.color='#433520');
-projNav.addEventListener('mouseout',()=>projNav.style.color='#433520');
-skillNav.addEventListener('mouseout',()=>skillNav.style.color='#433520');
+menav.addEventListener('mouseout',()=>menav.style.color='#EAEAEA');
+projNav.addEventListener('mouseout',()=>projNav.style.color='#EAEAEA');
+skillNav.addEventListener('mouseout',()=>skillNav.style.color='#EAEAEA');
