8e7e9d46e06607ec35c5fb54044f3947b1023fa9
filipmed <74942293+filipmed@users.noreply.github.com>
2022-05-18 03:45:02 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,34 +1,6 @@
-
-//This code does the same as above except that It is done in a more efficient manner while using my newly learned JQuerly Knowledge
-$("#me").mouseover(function(){
-    $("#me").css("color","#FF2E63")
-});
-
-$("#proj").mouseover(function(){
-    $("#proj").css("color","#FF2E63")
-});
-
-$("#skills").mouseover(function(){
-    $("#skills").css("color","#FF2E63")
-}); 
-
 $(".logo").mouseover(function(){
     $(this).css("color","#FF2E63")
 }); 
-
-
-
-$("#me").mouseout(function(){
-    $("#me").css("color","#EAEAEA")
-});
-$("#proj").mouseout(function(){
-    $("#proj").css("color","#EAEAEA")
-});
-$("#skills").mouseout(function(){
-    $("#skills").css("color","#EAEAEA")
-});
 $(".logo").mouseout(function(){
     $(this).css("color","#EAEAEA")
 });
-
-
